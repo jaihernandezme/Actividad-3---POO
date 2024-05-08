bb95e6cff21950799ae284d285fa196f9e4d98ea
jaihernandezme <160196595+jaihernandezme@users.noreply.github.com>
2024-05-07 23:20:59 -0500

    Add files via upload

---
+++ EJRES7.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.ejres7;
+    import Interfaz.Pantalla;
+public class EJRES7 {
+
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla ();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+    }
+}
